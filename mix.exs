c10ed85b6e9758cb827fecfb707ee225d0d6fc98
Jeff Smith <electricshaman@users.noreply.github.com>
2022-02-25 17:02:24 -0600

    Move mint dep under mint_web_socket for test

--- mix.exs
+++ mix.exs
@@ -91,8 +91,8 @@ defmodule Phoenix.MixProject do
       {:phoenix_html, "~> 3.0", only: [:docs, :test]},
       {:phx_new, path: "./installer", only: :test},
       {:websocket_client, git: "https://github.com/jeremyong/websocket_client.git", only: :test},
-      {:mint, "~> 1.4", only: :test},
       {:mint_web_socket, "~> 0.2.0", only: :test},
+      {:mint, "~> 1.4", only: :test},
 
       # Dev dependencies
       {:esbuild, "~> 0.3", only: :dev}
